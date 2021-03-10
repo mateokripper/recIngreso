3cd90d9463b2639464e1dfd318ec80c939e645c3
unknown <mateokripper27@gmail.com>
2021-03-10 11:04:39 -0300

    prueba

--- dos.js
+++ dos.js
@@ -1,4 +1,123 @@
 function mostrar()
+/*
+Realizar el algoritmo que permita ingresar los datos de los 500 alumnos de la UTN FRA, los datos solicitados son:
+nombre carrera("Quimica";"Fisica";"Sistemas")
+sexo (masculino - femenino - no binario)
+cantidad de materias(entre 1 y 5)
+Nota promedio (entre 0 y 10)
+edad (validar)
+se debe informar de existir, o informar que no existe , en el caso que corresponda.
+a) El nombre del mejor promedio de los alumnos que estudian Fisica. (Sin importar el sexo).
+b) El nombre de la alumna mas joven.
+c) Porcentaje de estudiantes que estudia cada carrera.
+d) La edad y nombre del estudiante que cursa mas materias exceptuando la carrera de Quimica.
+*/
 {
-    alert("Ejercicio 2");
+    let nombre;
+    let sexo;
+    let nombreCarrera;
+    let cantidadMat;
+    let notaProm;
+    let edad;
+    let nombreMPFisica;
+    let mejorProm;
+    let flagNMPFisica = 1;
+    let nombreAMasJoven;
+    let flagAMasJoven=1;
+    let menorEdad;
+    let flagMMaterias = 1;
+    let nombreEMM;
+    let edadEMM;
+    let mayorCantMat;
+    
+    let contFis=0;
+    let contQ=0;
+    let contSis=0;
+    let porcFis;
+    let porcQ;
+    let porcSis;
+   
+
+
+    for (let i=0; i < 500; i++){
+nombre=prompt("Ingrese el nombre del alumno: ");       
+nombreCarrera=prompt("Ingrese nombre de la carrera Quimica/Fisica/Sistemas: ");
+while(nombreCarrera!="Quimica" && nombreCarrera!="Fisica" && nombreCarrera!="sistemas"){
+    nombreCarrera=prompt("Error. Ingrese nombre de la carrera Quimica/Fisica/Sistemas: ");
 }
+sexo=prompt("Ingrese sexo masculino/femenino/no binario: ");
+while(sexo!="masculino" && sexo!="Fisica" && sexo!="sistemas"){
+    sexo=prompt("Error. Ingrese sexo masculino/femenino/no binario: ");
+}
+cantidadMat=parseInt(prompt("Ingrese hasta 5 materias: "));
+while(cantidadMat<0 && cantidadMat > 5 && isNaN(cantidadMat)){
+    cantidadMat=parseInt(prompt("Error. Ingrese hasta 5 materias: "));
+}
+notaProm=parseInt(prompt("Ingrese nota promedio hasta 10: "));
+while(notaProm <0  && notaProm > 10 && isNaN(notaProm)){
+    notaProm=parseInt(prompt("Error. Ingrese nota promedio hasta 10: "));
+}
+edad=parseInt(prompt("Ingrese edad : "));
+while(edad <0  && isNaN(edad)){
+    edad=parseInt(prompt("Error. Ingrese edad: "));
+}
+
+if (nombreCarrera=="Fisica"){
+	if(flagNMPFisica || notaProm > mejorProm){
+	    mejorProm=notaProm;
+        nombreMPFisica=nombre;
+	  
+	  flagNMPFisica=0;
+	}
+  }
+if(edad < menorEdad && flagAMasJoven  ){
+    menorEdad=edad;
+    nombreAMasJoven=nombre;
+    flagAMasJoven=0;
+}
+ if(nombreCarrera != "Quimica") {
+   if(cantidadMat>mayorCantMat){
+       mayorCantMat=cantidadMat;
+       nombreEMM=nombre;
+       edadEMM=edad;
+
+       flagMMaterias = 0;
+   }
+ }
+ if (nombreCarrera == "Fisica"){
+	contFis++;
+}else if (nombreCarrera== "Quimica"){
+	contQ++;
+}else {
+	contSis++;
+}
+
+}
+if(flagNMPFisica){
+	console.log("a- No se ingresaron alumnos que estudien fisica");
+
+}else{
+	console.log("a- el nombre del alumno con mejor promedio en fisica es: " + nombreMPFisica);
+}
+
+console.log("b- El nombre de la alumna mas joven es:" + nombreAMasJoven);
+
+if(contFis!=0){
+    porcFis= (contFis * 100) / 500;
+}
+if(contQ !=0){
+    porcQ= (contQ*100)/500;
+
+}
+if(contSis!=0){
+    porcSis=(contSis*100)/500;
+}
+console.log(`c- Porcentaje de estudiantes que estudia cada carrera: fisica ${porcFis}  quimica ${porcQ}  sistemas ${porcSis}`);
+
+if(edadEMM){
+	console.log("d- No se ingresaron alumnos que no estudien quimica.");
+
+}else{
+	console.log("d- la edad y nombre del estudiante que cursa mas materias exceptuando la carrera de Quimica es : " + nombreEMM + edadEMM);
+}
+}    
--- uno.js
+++ uno.js
@@ -1,5 +1,126 @@
-
+/*Una software factory registra la siguiente informacion de sus empleados:
+Nombre edad (validar)
+sexo (masculino - femenino - no binario)
+puesto (programador - analista - Qa)
+sueldo (entre 15000 y 70000)
+La empresa desea saber: se debe informar de existir, o informar que no existe , en el caso que corresponda.
+a) promedio de sueldos para cada puesto
+b) el sexo del que percibe el mayor sueldo
+c) el nombre del empleado femenino con mas sueldo
+d) cantidad de programadores no binarios que cobran sueldos entre 20000 y 55000
+*/
 function mostrar()
 {
-	alert("Ejercicio 1");
+	let nombre;
+	let puesto;
+	let sexo;
+	let edad;
+	let sueldo;
+	let seguir;
+	let acumPr= 0;
+    let acumAn =0;
+    let acumQa=0;
+    let contPr=0;
+    let contAn=0;
+    let contQa=0;
+    let promPr=0;
+    let promAn=0;
+    let promQa=0;
+	let sexoMayorSueldo;
+	let flagSexo=1;
+	let mayorSueldo;
+	let nombreFemMSueldo;
+	let flagNombre=1;
+	let mayorSueldoF;
+	let contPrNB = 0;
+    let flagNB = 1;
+
+
+do{
+nombre=prompt("Ingrese nombre: ");
+edad=parseInt(prompt("Ingrese edad: "));
+while(isNaN(edad) && edad < 0 ){
+	edad=parseInt(prompt("Error. Ingrese edad: "));
+}
+sexo=prompt("Ingrese sexo masculino/femenino/no binario: ");
+while(sexo != "masculino" && sexo != "femenino" && sexo != "no binario"){
+	sexo=prompt("Error. Ingrese sexo: ");
+}
+puesto=prompt("Ingrese puesto programador/analista/Qa: ");
+while(puesto != "programador" && puesto != "analista" && puesto != "Qa"){
+	puesto=prompt("Error. Ingrese puesto programador/analista/Qa: ");
+}
+sueldo = parseInt(prompt("Ingrese sueldo entre 15000 y 70000: "));
+while(sueldo < 15000 && sueldo > 70000){
+	sueldo = parseInt(prompt("Error. Ingrese sueldo entre 15000 y 70000: "));
+}
+
+if (puesto == "programador"){
+	acumPr+=cantidad;
+	contPr++;
+}else if (tipo == "analista"){
+	acumAn+=cantidad;
+	contAn++;
+}else {
+	acumQa+=cantidad;
+	contQa++;
+}
+
+if (flagSexo || sueldo> mayorSueldo ){
+	mayorSueldo=sueldo;
+	sexoMayorSueldo=sexo;
+	
+
+	flagSexo = 0;
+}
+
+if (sexo=="femenino"){
+	if(flagNombre || sueldo > mayorSueldoF){
+	  mayorSueldoF=sueldo;
+		nombreFemMSueldo=nombre;
+	  
+	  flagLibre=0;
+	}
+  }
+  if (flagNB && sexo== "no binario"){
+	if(sueldo >= 20000 && sueldo <= 55000){
+		contPrNB ++; 
+        flagNB=0;
+	}
+}
+seguir=prompt("Desea ingresar otro empleado?");
+}
+while(seguir=="s");
+
+if(contPr!=0){
+    promPr=acumPr/contPr;
+}
+if (contAn!=0){
+    promAn=acumAn/contAn;
+}
+if(contQa!=0){
+    promQa=acumQa/contQa;
+}
+
+
+console.log(`A- El promedio de sueldo por cada puesto es: programador ${promPr}  analista ${promAn}  Qa  ${promQa}` );
+console.log("B- El sexo del empleado con mayor sueldo es:" + sexoMayorSueldo);
+if(flagNombre){
+	console.log("C- No se ingresaron empleados femeninos");
+
+}else{
+	console.log("C- el nombre del empleado femenino con mayor sueldo es: " + nombreFemMSueldo)
+}
+if(flagNB){
+	console.log("D- no se ingresaron empleados no binarios. ");
+}else{
+	console.log("D- La cantidad de empleados no binarios que cobran entre 20000 y 55000 es: " + contPrNB);
+
+
+
+
+
+
+
+
 }
